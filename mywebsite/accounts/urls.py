f66c161af946c9468c978b840f0569be662d4db4
George Ellis <george.ellis.23@bishops.com>
2021-04-26 11:26:06 -0700

    Made it so navbar links go to different pages (need to fix profile page still), and changed navbar colors.

--- mywebsite/accounts/urls.py
+++ mywebsite/accounts/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('profile/<str:pk>/', views.profile),
-    path('settings/', views.settings),
-    path('feed/', views.feed),
-    path('login/', views.login),
+    path('', views.home, name="home_page"),
+    path('profile/<str:pk>/', views.profile, name="profile_page"),
+    path('settings/', views.settings, name="settings_page"),
+    path('feed/', views.feed, name="feed_page"),
+    path('login/', views.login, name="login_page"),
 
 ]
