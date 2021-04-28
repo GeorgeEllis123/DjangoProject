05bb8eb20f13a3e34d2a1a676c57f4dcd5a3c10c
George Ellis <george.ellis.23@bishops.com>
2021-04-28 11:31:45 -0700

    Added images to post model

--- mywebsite/accounts/models.py
+++ mywebsite/accounts/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=50, null=True)
     caption = models.CharField(max_length=200, null=True)
-    #image = asdf
+    image = models.ImageField(null=True, blank=True)
     post_date = models.DateTimeField('date published')
     userPosted = models.ForeignKey(Profile, null=True, on_delete=models.CASCADE)
     likes = models.IntegerField(default=0)
