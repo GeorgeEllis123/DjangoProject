5618f4d6035e4caedd4cd86740b1c6c686ca86f0
George Ellis <george.ellis.23@bishops.com>
2021-05-06 14:23:18 -0700

    Made it so profile model was created on user creation

--- mywebsite/accounts/models.py
+++ mywebsite/accounts/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     profile_pic = models.ImageField(null=True, blank=True)
     bio = models.TextField(default="", blank=True)
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    post_save.connect(create_user_profile, sender=User)
 
     #def __str__(self):
     #    return self.user
